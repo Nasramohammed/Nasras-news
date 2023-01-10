53e3e81ab8b471db8381a1b6b32d9f7741b0d994
Nasramohammed <nasra_mohammed@hotmail.co.uk>
2023-01-10 21:36:22 +0000

    complete ticket 6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './Navbar.css';
 
 function Navbar() {
   return (
     <div>
       <nav>
-        <ul>
+        <ul className="navbar_list">
           <li>Home</li>
             <li>topics</li>
             <li>users</li>
